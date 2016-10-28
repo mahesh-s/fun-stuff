74463fba736c234ee47153fc90be0c417509c468
Mahesh Singareddy <mahesh.singareddy@gmail.com>
2016-10-28 11:52:07 -0500

    Adding gitignore and RFP

---
+++ HomeSurfer/HomeSurferModels/RFP.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeSurferModels
+{
+    public class RFP
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }        
+    }
+}
